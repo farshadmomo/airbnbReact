635b542a541c8079c7022929f6931933b1cff34c
farshadmomo <momtazfarshad@gmail.com>
2023-08-29 18:14:13 +0330

    profiles

--- src/App.js
+++ src/App.js
@@ -2,15 +2,6 @@ import React from "react";
 import "./App.css"
 import NavBar from "./components/NavBar";
 import Card from "./components/Card";
-// import profile1 from "../public/images/albert-dera-ILip77SbmOE-unsplash.jpg"
-// import profile2 from "../public/images/ayo-ogunseinde-6W4F62sN_yI-unsplash.jpg"
-// import profile3 from "../public/images/brooke-cagle-4gfbg7IulGA-unsplash.jpg"
-// import profile4 from "../public/images/slav-romanov-BrEAp01_m5w-unsplash.jpg"
-// import profile5 from "../public/images/jurica-koletic-7YVZYZeITc8-unsplash.jpg"
-// import profile6 from "../public/images/joseph-gonzalez-iFgRcqHznqg-unsplash.jpg"
-// import profile7 from "../public/images/charles-etoroma-95UF6LXe-Lo-unsplash.jpg"
-// import profile8 from "../public/images/matheus-ferrero-W7b3eDUb_2I-unsplash.jpg"
-
 
 function App() {
     return (
