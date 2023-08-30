e4dba0d8a6302f1b1b0da3d2ca37ddccb28a0442
farshadmomo <momtazfarshad@gmail.com>
2023-08-30 12:08:31 +0330

    props

--- src/App.js
+++ src/App.js
@@ -2,48 +2,25 @@ import React from "react";
 import "./App.css"
 import NavBar from "./components/NavBar";
 import Card from "./components/Card";
+import Data from "./data";
 
 function App() {
+
+    const cards = Data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                item={item}
+            />
+        )
+    })
+
     return (
         <div className='container'>
             <NavBar/>
             {/*<Hero />*/}
             <div className="cardContainer">
-            <Card
-                img='/images/profile1.jpg'
-                rating="5.0"
-                votes="6"
-                country="USA"
-                title="life lessons with albert dera"
-                price="136"/>
-            <Card
-                img='/images/profile2.jpg'
-                rating="3.0"
-                votes="15"
-                country="UK"
-                title="yoga with oganj"
-                price="220"/>
-            <Card
-                img='/images/profile3.jpg'
-                rating="4.0"
-                votes="16"
-                country="IR"
-                title="bussines with brookie cagle"
-                price="330"/>
-            <Card
-                img='/images/profile4.jpg'
-                rating="4.5"
-                votes="4"
-                country="DE"
-                title="photography lessons with slav romanov"
-                price="136"/>
-            <Card
-                img='/images/profile5.jpg'
-                rating="3.5"
-                votes="30"
-                country="IN"
-                title="programming lessons with jurcia"
-                price="100"/>
+                {cards}
             </div>
         </div>
   )
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,17 +2,24 @@ import React from "react"
 // import starIcon from "../../public/images/icons8-star-30.png"
 
 export default function Card(props) {
+    let badgeText
+    if (props.item.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.item.location === "Online") {
+        badgeText = "ONLINE"
+    }
     return (
         <div className="card">
-            <img src={props.img} alt='profile' className="card--image"/>
+            {badgeText && <div className='soldout'>{badgeText}</div>}
+            <img src={props.item.coverImg} alt='profile' className="card--image"/>
             <div className="card--stats">
                 <img src='/images/icons8-star-30.png' alt='starIcon' className="card--star"/>
-                <span>{props.rating}</span>
-                <span className="gray">({props.votes}) • </span>
-                <span className="gray">{props.country}</span>
+                <span>{props.item.stats.rating}</span>
+                <span className="gray">({props.item.stats.reviewCount}) • </span>
+                <span className="gray">{props.item.location}</span>
             </div>
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <p>{props.item.title}</p>
+            <p><span className="bold">From ${props.item.price}</span> / person</p>
         </div>
     )
 }
